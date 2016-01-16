334dd5fd5108964f2e803ead5bae28c687de513c
Erik Carlsson <info@code301.com>
2016-01-16 21:37:26 +0000

    Init

---
+++ lib/Dist/Zilla/Plugin/CoverToMeta.pm
@@ -0,0 +1,28 @@
+use 5.14.0;
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::CoverToMeta;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Plugin::CoverToMeta;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::Plugin::CoverToMeta is ...
+
+=head1 SEE ALSO
+
+=cut
